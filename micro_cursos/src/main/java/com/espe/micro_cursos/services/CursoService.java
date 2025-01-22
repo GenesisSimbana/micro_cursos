842df481c917623c1a051033306942737f3d8233
Genesis Carolina Simbaña Chaza <107575284+GenesisSimbana@users.noreply.github.com>
2025-01-22 10:12:32 -0500

    Add files via upload

---
+++ micro_cursos/src/main/java/com/espe/micro_cursos/services/CursoService.java
@@ -0,0 +1,18 @@
+package com.espe.micro_cursos.services;
+import java.util.Optional;
+import java.util.List;
+
+import com.espe.micro_cursos.models.Usuario;
+import com.espe.micro_cursos.models.entities.Curso;
+
+
+public interface CursoService {
+
+    List<Curso> findAll();
+    Optional<Curso> findById(Long id);
+    Curso save(Curso curso);
+    void deleteById(Long id);
+
+    Optional <Usuario> addUsuario(Usuario usuario, Long id);
+
+}
